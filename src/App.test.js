0cfd99da57ae46d058ad47f28efc39634ddcb774
byalif <alif.rahi68@qmail.cuny.edu>
2024-01-15 21:27:35 -0500

    added stuff3

--- src/App.test.js
+++ src/App.test.js
@@ -2,3 +2,8 @@ it("sums numbers", () => {
   expect(1 + 2).toEqual(3);
   expect(2 + 2).toEqual(4);
 });
+
+it("sums numbers", () => {
+  expect(1 + 2).toEqual(3);
+  expect(2 + 2).toEqual(4);
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,5 @@
-// react-testing-library renders your components to document.body,
-// this will ensure they're removed after each test.
-import "react-testing-library/cleanup-after-each";
-
-// this adds jest-dom's custom assertions
-import "jest-dom/extend-expect";
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
